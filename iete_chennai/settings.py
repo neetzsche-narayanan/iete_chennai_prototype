cb57729f4895f87140e8d9aa1adb549a445522ce
NEETzche <129645930+NEETzche@users.noreply.github.com>
2023-09-03 18:06:55 +0530

    Update settings.py

--- iete_chennai/settings.py
+++ iete_chennai/settings.py
@@ -24,7 +24,7 @@ environ.Env.read_env()
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET KEY REMOVED BY YOU.
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 import os
