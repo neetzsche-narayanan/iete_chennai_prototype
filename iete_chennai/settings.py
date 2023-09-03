30175f9261c418ff69b2d6aa21a8cfff1024a303
NEETzche <129645930+NEETzche@users.noreply.github.com>
2023-09-03 18:51:21 +0530

    Update settings.py

--- iete_chennai/settings.py
+++ iete_chennai/settings.py
@@ -29,12 +29,9 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 import os
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-if RENDER_EXTERNAL_HOSTNAME:
-    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
