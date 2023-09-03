1eb63a2a3cb5fdd7748df0051c1839a489a9f405
NEETzche <129645930+NEETzche@users.noreply.github.com>
2023-09-03 22:50:06 +0530

    Update settings.py

--- iete_chennai/settings.py
+++ iete_chennai/settings.py
@@ -82,20 +82,18 @@ WSGI_APPLICATION = 'iete_chennai.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Render PostgreSQL Database (live)
-
-import dj_database_url
-
-DATABASES  = {
-    'default': dj_database_url.parse(env('DATABASE_URL'))
-}
+#import dj_database_url
+#DATABASES  = {
+#    'default': dj_database_url.parse(env('DATABASE_URL'))
+#}
 
 
 
